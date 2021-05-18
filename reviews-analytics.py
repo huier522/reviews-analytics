bd7f01822e84475b36dafa85a5effcdf590210f7
huier <andychen522.ambit@gmail.com>
2021-05-18 11:17:01 +0800

    add average reviews length function and .gitignore

--- reviews-analytics.py
+++ reviews-analytics.py
@@ -1,6 +1,5 @@
 data = []
 count = 0
-
 with open('reviews.txt', 'r') as f:
     for line in f:
         data.append(line)
@@ -8,3 +7,9 @@ with open('reviews.txt', 'r') as f:
         if count % 100000 == 0:
             print(len(data))
 print(f'檔案讀取完畢，共有{len(data)}筆資料。')
+
+sum_review = 0
+for review in data:
+    sum_review += len(review)
+avg_review = sum_review / len(data)
+print(f'每筆留言平均長度：{avg_review}')
