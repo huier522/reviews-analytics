23336a9de2bb7baea7e5c217910555901918c9da
huier <andychen522.ambit@gmail.com>
2021-05-19 00:43:41 +0800

    modify to list comprehension

--- reviews-analytics.py
+++ reviews-analytics.py
@@ -14,8 +14,20 @@ for review in data:
 avg_review = sum_review / len(data)
 print(f'每筆留言平均長度：{avg_review}')
 
-words_list = []
-for words in data:
-    if len(words) < 100:
-        words_list.append(len(words))
+# words_list = []
+# for words in data:
+#     if len(words) < 100:
+#         words_list.append(len(words))
+#List Comprehonsion 清單快寫法
+words_list = [len(words) for words in data if len(words) < 100]
 print(f'留言長度小於100的總共有{len(words_list)}筆。')
+
+# good_list = []
+# for good in data:
+#     if 'good' in good:
+#         good_list.append(good)
+#List Comprehonsion 清單快寫法
+good_list = [good for good in data if 'good' in good]
+print(f'留言裡有提到good的總共有{len(good_list)}筆。')
+bad_list = [bad for bad in data if 'bad' in bad]
+print(f'留言裡有提到bad的總共有{len(bad_list)}筆。')
